9d3768412420c24bf9b651c10a60657ac1c69f0d
David <ik.ben.david@hotmail.com>
2018-06-19 13:00:26 +0200

    first push, the code is working in wordpress. I need to adjust the style and make a blog page

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php get_header();?>
+<body>
+<div class="container">
+    <header>
+        <h1>Wordpress</h1>
+    </header>
+    <main>
+        <h2>Main</h2>
+    </main>
+    <?php get_sidebar();
+    get_footer(); ?>
---
+++ sidebar.php
@@ -0,0 +1,9 @@
+<aside>
+    <h2>Aside</h2>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li>Blog</li>
+        <li><a href="index.php">Werkt niet</a></li>
+        <li><a href="index.php">Werkt niet</a></li>
+    </ul>
+</aside>
